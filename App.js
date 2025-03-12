a3621e130c4dc487b877b4a4bac19bd493c5437e
Nisa Aydın <nisaayddin@gmail.com>
2025-03-12 13:42:28 -0700

    App.js modified

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,Image ,TouchableWithoutFeedback ,TouchableOpacity} from 'react-native';
-import { SafeAreaView } from 'react-native-web';
+import { StyleSheet, Text, View,Image ,TouchableWithoutFeedback ,TouchableOpacity,TouchableHighlight,Alert,Pressable} from 'react-native';
+import { Button, SafeAreaView, ScrollView } from 'react-native-web';
 
 export default function App() {
   
@@ -8,32 +8,56 @@ export default function App() {
 
 
     <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <Button color='orange' style={buttonStyle} title="Click Me" onPress={()=>{alert("Somebody pressed the button ! Call the police !")}} />
+        <Button color='red' title="Click Me" onPress={()=>Alert.alert("My Title","My message",
+          [{ text:"Yes" },
+          { text:"No" }])} />
+
+<Button
+          color="blue"
+          title="Message"
+          onPress={() =>
+            Alert.prompt("My Title", "My message", (text) =>
+              console.log("Entered text:", text)
+            )
+          }
+        />
       <Text>Hello!</Text>
       <StatusBar style="auto" />
-      <TouchableWithoutFeedback onPress={()=>{console.log("image tapped")}}>
+      <Pressable onPress={()=>{console.log("image tapped")}}>
       <Image blurRadius={2} source={{
           width:200,
           height:300,
           uri:"https://picsum.photos/seed/picsum/200/300"}}/>
-      </TouchableWithoutFeedback>
+      </Pressable>
+
       <TouchableOpacity onPress={()=>{console.log("image tapped")}}>
       <Image source={{
           width:200,
           height:300,
           uri:"https://picsum.photos/200/300?grayscale"}}/>
-
-
       </TouchableOpacity>
 
+      <TouchableHighlight onPress={()=>{console.log("image tapped")}} >
+      <Image source={{
+          width:200,
+          height:300,
+          uri:"https://picsum.photos/id/237/200/300"}}/>
+      </TouchableHighlight>
+      </ScrollView>
     </SafeAreaView>
   );
 }
 
+const buttonStyle = {color:"black"};
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex:1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop:50,
   },
 });
